e1b9a60b53e2197b9c8d98ea241f47cdff7b204a
Alex Luque <alejandro.golu@gmail.com>
2022-07-18 17:26:24 +0200

    Feat: created tasks view
    This is the view to show all the existing tasks so they could be edited, deleted or run.
    The view also allows the user to search for a task by typing its name on the search bar and to order the tasks by name or creation date, sorting them in ascending or descending order.
    Also, a plus button to create a new tasks is presented on the top right corner.
    
    Closes #4

--- RunIt/RunIt/Views/TasksView.swift
+++ RunIt/RunIt/Views/TasksView.swift
@@ -11,6 +11,30 @@ struct TasksView: View {
     @EnvironmentObject var dataController: DataController
     let tasks: FetchRequest<Task>
     
+    @State var sortByName = true
+    @State var sortAscending = true
+    @State var canCreateTask = false
+    @State var searchedTaskName = ""
+    @State var canBeDeleted = false
+    
+    var searchedTasks: [Task] {
+        var sortedTasks = tasks.wrappedValue.sorted(by: {
+            if sortByName {
+                return $0.taskName < $1.taskName
+            } else {
+                return $0.creationDate < $1.creationDate
+            }
+        })
+        
+        sortedTasks = sortAscending ? sortedTasks : sortedTasks.reversed()
+        
+        if searchedTaskName.isEmpty {
+            return sortedTasks.map({ $0 })
+        } else {
+            return sortedTasks.filter({ $0.taskName.contains(searchedTaskName) })
+        }
+    }
+    
     init() {
         tasks = FetchRequest<Task>(
             entity: Task.entity(),
@@ -18,29 +42,123 @@ struct TasksView: View {
         )
     }
     
+    var sortingSelector: some View {
+        HStack {
+            Picker("Sorted by", selection: $sortByName) {
+                Text("Name").tag(true)
+                Text("Creation date").tag(false)
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            
+            Spacer()
+            
+            Button {
+                sortAscending.toggle()
+            } label: {
+                Image(systemName: sortAscending ? "arrow.down" : "arrow.up")
+            }
+        }
+    }
+    
+    var newTaskButton: some View {
+        Button {
+            canCreateTask.toggle()
+        } label: {
+            Image(systemName: "plus")
+        }
+        .sheet(isPresented: $canCreateTask) {
+            TaskView()
+        }
+    }
+    
     var body: some View {
         NavigationView {
-            List {
-                ForEach(tasks.wrappedValue) { task in
-                    Section(header: Text(task.name ?? "")) {
-                        ForEach(task.steps?.allObjects as? [Step] ?? []) { step in
-                            Text(step.name ?? "")
+            VStack {
+                VStack(alignment: .leading, spacing: 5) {
+                    Text("Sorted by:")
+                        .font(.caption)
+                    
+                    sortingSelector
+                }
+                .padding()
+                
+                List {
+                    ForEach(searchedTasks) { task in
+                        Section(header: TaskHeader(task: task)) {
+                            TaskBody(task: task)
+                        }
+                    }
+                    .onDelete { _ in
+                        canBeDeleted = true
+                    }
+                    .alert("Do you want to delete the task?", isPresented: $canBeDeleted) {
+                        Button("Delete", role: .destructive) {
+                            
                         }
                     }
                 }
+                .listStyle(.insetGrouped)
+                .searchable(text: $searchedTaskName, prompt: "Type the name of the task")
             }
-            .listStyle(.insetGrouped)
             .navigationTitle("Tasks")
             .toolbar {
                 ToolbarItem {
-                    Button {
-                        
-                    } label: {
-                        Image(systemName: "plus")
-                    }
+                    newTaskButton
                 }
             }
         }
+    }
+}
+
+struct TaskHeader: View {
+    let task: Task
+    
+    @State var canEditTask = false
+    
+    var body: some View {
+        HStack(alignment: .top) {
+            Text(task.taskName)
+            
+            Spacer()
+            
+            Button {
+                canEditTask.toggle()
+            } label: {
+                Image(systemName: "square.and.pencil")
+            }
+            .sheet(isPresented: $canEditTask) {
+                TaskView(task: task)
+            }
+            .padding(.trailing)
+            
+            Button {
+                
+            } label: {
+                Image(systemName: "play.fill")
+            }
+        }
+    }
+}
+
+struct TaskBody: View {
+    let task: Task
+    
+    var body: some View {
+        HStack {
+            let stepsCount = task.taskSteps.count
+            let form = stepsCount == 1 ? "Step" : "Steps"
+            Text("\(stepsCount) \(form)")
+            
+            Spacer()
+            
+            let length = task.taskSteps.map({ $0.length }).reduce(0, +)
+            Text(length == 0 ? "No length set" : "\(length) Sec.")
+            
+            Spacer()
+            
+            Text(task.creationDate.format())
+        }
+        .font(.footnote)
     }
 }
 
