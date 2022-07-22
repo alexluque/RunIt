27e173b0582c1f37589b30e5ac6e1908f8fbf0d6
Alex Luque <alejandro.golu@gmail.com>
2022-07-22 17:58:13 +0200

    Feat: fake task for testing purposes.
    I've created a simple task so it can be used easily on previews.

--- RunIt/RunIt/Extensions/TaskExtensions.swift
+++ RunIt/RunIt/Extensions/TaskExtensions.swift
@@ -24,4 +24,28 @@ extension Task {
     var taskSteps: [Step] {
         steps?.allObjects as? [Step] ?? []
     }
+    
+    static var example: Task {
+        let controller = DataController(inMemory: true)
+        let viewContext = controller.container.viewContext
+        
+        let task = Task(context: viewContext)
+        task.name = "Example task"
+        task.areStepsOrdered = Bool.random()
+        task.creation = Date.now
+        task.steps = []
+        task.length = 0
+        
+        for stepNumber in 1...4 {
+            let step = Step(context: viewContext)
+            step.name = "Step \(task.taskName)-\(stepNumber)"
+            step.length = stepNumber == 4 ? 0 : Int16(stepNumber * 10)
+            step.tasks = [task]
+            step.lastStepIn = nil
+            
+            task.length += step.length
+        }
+        
+        return task
+    }
 }
